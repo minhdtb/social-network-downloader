ab932846afc8280f1792e657cedcb5f6eb9b1cfd
minhdtb <minhdtb@evolableasia.vn>
2019-12-20 10:18:49 +0700

    update coding

--- plugins/instagram.go
+++ plugins/instagram.go
@@ -1,5 +1,10 @@
 package plugins
 
+import (
+	"regexp"
+	"strings"
+)
+
 type Instagram struct {
 }
 
@@ -14,13 +19,33 @@ func (r Instagram) GetType() int32 {
 }
 
 func (r Instagram) GetTitle(content string) *string {
+	regex2, _ := regexp.Compile(`property="og:title" content="([^"]+)"`)
+	match := regex2.FindStringSubmatch(content)
+	if match != nil && len(match) > 1 {
+		return &match[1]
+	}
+
 	return nil
 }
 
 func (r Instagram) GetThumbnail(content string) *string {
+	regex2, _ := regexp.Compile(`property="og:image" content="([^"]+)"`)
+	match := regex2.FindStringSubmatch(content)
+	if match != nil && len(match) > 1 {
+		return &match[1]
+	}
+
 	return nil
 }
 
 func (r Instagram) GetVideoUrl(content string) *string {
+	regex2, _ := regexp.Compile(`property="og:video" content="([^"]+)"`)
+	match := regex2.FindStringSubmatch(content)
+	if match != nil && len(match) > 1 {
+		var str = match[1]
+		str = strings.Replace(str, "&amp;", "&", -1)
+		return &str
+	}
+
 	return nil
 }
